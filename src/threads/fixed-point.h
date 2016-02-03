b3d85b52ffc2d80d0d4fc3cd9dad0dfe9c3d7ad5
srufe001 <srufe001@ucr.edu>
2016-02-02 18:31:05 -0800

    did some work on fixed-point.h

--- src/threads/fixed-point.h
+++ src/threads/fixed-point.h
@@ -1,21 +1,31 @@
-#ifndef THREADS_FIXED-POINT_H
-#define THREADS_FIXED-POINT_H
+#ifndef THREADS_FIXED_POINT_H
+#define THREADS_FIXED_POINT_H
+
+#include<iostream>
+#include<stdint.h>
+
+
+unsigned char p = 17;
+unsigned char q = 14;
+unsigned char f = 1 << q;
 
 // the fixed-point numbers in this file are 17.14
 // x and y refer to fixed-point numbers, n refers to a integer
 
-typedef int32_t fixed
+typedef int32_t fixed;
 
 // Converts an integer n to a fixed-point number
 fixed
 toFP(int n)
 {
+  return n * f;
 }
 
 // Converts a fixed-point number x to an integer, rounding toward zero
 int
 toIntZero(fixed x)
 {
+  return x / f;
 }
 
 // Converts a fixed-point number x to an integer, rounding to nearest integer
@@ -28,6 +38,7 @@ toIntRound(fixed x)
 fixed
 addFF(fixed x, fixed y)
 {
+  return x + y;
 }
 
 // Subtracts a fixed-point number y from a fixed-point number x
@@ -40,6 +51,7 @@ subFF(fixed x, fixed y)
 fixed
 addFI(fixed x, int n)
 {
+  return x + toFP(n);
 }
 
 // Subtract an integer n from a fixed-point number x
@@ -52,12 +64,14 @@ subFI(fixed x, int n)
 fixed
 multFF(fixed x, fixed y)
 {
+  return ((int64_t) x) * y / f;
 }
 
 // Multiply a fixed-point number x by an integer n
 fixed
 multFI(fixed x, int n)
 {
+  return x * n;
 }
 
 // Divide a fixed-point number x by a fixed-point number y
@@ -70,6 +84,21 @@ divFF(fixed x, fixed y)
 fixed
 divFI(fixed x, int n)
 {
+}
+
+int main() {
+  int a_i = 16;
+  fixed a_f = toFP(a_i);
+  int b_i = 8;
+  fixed b_f = toFP(b_i);
+
+  std::cout << toIntZero(addFI(a_f, b_i)) << std::endl;
+  std::cout << toIntZero(addFF(a_f, b_f)) << std::endl;
+
+  std::cout << toIntZero(multFI(a_f, b_i)) << std::endl;
+  std::cout << toIntZero(multFF(a_f, b_f)) << std::endl;
+
+  return 0;
 }
 
 #endif /* threads/fixed-point.h */
