16319dec39dba2b2b1b259a5ecda7759f8d02f51
srufe001 <srufe001@ucr.edu>
2016-02-02 20:24:23 -0800

    finished fixed-point.h

--- src/threads/fixed-point.h
+++ src/threads/fixed-point.h
@@ -28,6 +28,10 @@ toIntZero(fixed x)
 int
 toIntRound(fixed x)
 {
+  if (x >= 0)
+    return (x + f / 2) / f;
+  else
+    return (x - f / 2) / f;
 }
 
 // Add 2 fixed-point number, x and y
@@ -41,6 +45,7 @@ addFF(fixed x, fixed y)
 fixed
 subFF(fixed x, fixed y)
 {
+  return x - y;
 }
 
 // Add a fixed-point number x to an integer n
@@ -54,6 +59,7 @@ addFI(fixed x, int n)
 fixed
 subFI(fixed x, int n)
 {
+  return x - toFP(n);
 }
 
 // Multiply a fixed-point number x by a fixed-point number y
@@ -74,12 +80,14 @@ multFI(fixed x, int n)
 fixed
 divFF(fixed x, fixed y)
 {
+  return ((int64_t) x) * f / y;
 }
 
 // Divide a fixed-point number x by an integer n
 fixed
 divFI(fixed x, int n)
 {
+  return x / n;
 }
 
 #endif /* threads/fixed-point.h */
